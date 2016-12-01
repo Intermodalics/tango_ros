8e1022686c630faa897faf71c1732f4bdb7b9190
Perrine Aguiar <perrine.aguiar@intermodalics.eu>
2016-12-01 10:56:50 +0100

    Clean up unused dependency

--- RosApp/app/src/main/jni/Android.mk
+++ RosApp/app/src/main/jni/Android.mk
@@ -9,7 +9,7 @@ LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
 LOCAL_CFLAGS  += -g3 -ggdb --std=c++11 -pthread -fPIC -fexceptions -frtti
 LOCAL_LDLIBS += -landroid -lm -llog
 LOCAL_STATIC_LIBRARIES += roscpp_android_ndk
-LOCAL_SHARED_LIBRARIES := tango_client_api tango_support_api tango_ros_native tango_ros_android
+LOCAL_SHARED_LIBRARIES := tango_client_api tango_support_api tango_ros_native
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
