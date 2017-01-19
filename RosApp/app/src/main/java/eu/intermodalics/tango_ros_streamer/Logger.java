071c453709bb2e5563ecb152b32cf5675cbbd632
Perrine Aguiar <perrine.aguiar@intermodalics.eu>
2017-01-19 13:36:46 +0100

    Rename app tango ros streamer

--- RosApp/app/src/main/java/eu/intermodalics/tango_ros_streamer/Logger.java
+++ RosApp/app/src/main/java/eu/intermodalics/tango_ros_streamer/Logger.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package eu.intermodalics.tangoxros;
+package eu.intermodalics.tango_ros_streamer;
 
 import android.app.Activity;
 import android.os.Environment;
