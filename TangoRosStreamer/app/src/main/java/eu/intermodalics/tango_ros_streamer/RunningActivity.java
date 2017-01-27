294b005f96e253133f97a98f7fc51afc98065110
Perrine Aguiar <perrine.aguiar@intermodalics.eu>
2017-01-27 10:34:21 +0100

    Correct nit

--- TangoRosStreamer/app/src/main/java/eu/intermodalics/tango_ros_streamer/RunningActivity.java
+++ TangoRosStreamer/app/src/main/java/eu/intermodalics/tango_ros_streamer/RunningActivity.java
@@ -73,7 +73,7 @@ public class RunningActivity extends AppCompatRosActivity implements TangoRosNod
 
     private SharedPreferences mSharedPref;
     private TangoRosNode mTangoRosNode;
-    private boolean runLocalMaster = false;
+    private boolean mRunLocalMaster = false;
     private String mMasterUri = "";
     private ParameterNode mParameterNode;
     private RosStatus mRosStatus = RosStatus.MASTER_NOT_CONNECTED;
@@ -214,7 +214,7 @@ public class RunningActivity extends AppCompatRosActivity implements TangoRosNod
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mSharedPref = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
-        runLocalMaster = mSharedPref.getBoolean(getString(R.string.pref_master_is_local_key), false);
+        mRunLocalMaster = mSharedPref.getBoolean(getString(R.string.pref_master_is_local_key), false);
         mMasterUri = mSharedPref.getString(getString(R.string.pref_master_uri_key),
                 getResources().getString(R.string.pref_master_uri_default));
         String logFileName = mSharedPref.getString(getString(R.string.pref_log_file_key),
@@ -282,7 +282,7 @@ public class RunningActivity extends AppCompatRosActivity implements TangoRosNod
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == RESULT_CANCELED) { // Result code returned when back button is pressed.
             if (requestCode == startSettingsActivityRequest.FIRST_RUN) {
-                runLocalMaster = mSharedPref.getBoolean(getString(R.string.pref_master_is_local_key), false);
+                mRunLocalMaster = mSharedPref.getBoolean(getString(R.string.pref_master_is_local_key), false);
                 mMasterUri = mSharedPref.getString(getString(R.string.pref_master_uri_key),
                         getResources().getString(R.string.pref_master_uri_default));
                 mUriTextView.setText(mMasterUri);
@@ -363,7 +363,7 @@ public class RunningActivity extends AppCompatRosActivity implements TangoRosNod
      * This function initializes the tango ros node with RosJava interface.
      */
     private void initAndStartRosJavaNode() {
-        if (runLocalMaster) {
+        if (mRunLocalMaster) {
             this.nodeMainExecutorService.startMaster(/*isPrivate*/ false);
             mMasterUri = this.nodeMainExecutorService.getMasterUri().toString();
             mUriTextView = (TextView) findViewById(R.id.master_uri);
