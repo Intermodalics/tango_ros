2f4bb75c45f436ce8d30a81b5d9acf99d45dd6dc
Perrine Aguiar <perrine.aguiar@intermodalics.eu>
2017-06-28 16:56:18 +0200

    Remove commented code

--- tango_ros_common/tango_ros_native/src/tango_ros_conversions_helper.cpp
+++ tango_ros_common/tango_ros_native/src/tango_ros_conversions_helper.cpp
@@ -279,7 +279,6 @@ void toOccupancyGrid(const Tango3DR_ImageBuffer& image_grid,
       // flipped around the x axis.
       int value = static_cast<int>(
           image_grid.data[j + (image_grid.height - i - 1) * image_grid.width]);
-      //occupancy_grid->data.push_back(-100 * value / 255 + 100);
       if (value < 128) {
         occupancy_grid->data.push_back(OCCUPIED_CELL);
       } else if (value > 128) {
